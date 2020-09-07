72e926cbb80994615db77b2e6b24cb89b2198299
lengyuedong <lengyuedong@doodlemobile.com>
2020-09-07 19:00:22 +0800

    first commit

---
+++ app/src/main/java/com/example/doodle/myapplication/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.doodle.myapplication;
+
+import android.app.Application;
+
+/**
+ * Created by ${Lengyuedong} on 2020/9/7.
+ */
+
+public class MyApplication extends Application {
+
+}
